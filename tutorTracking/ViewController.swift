7cad6e1638db861d0329c634302e012050b8d114
Brian O'Leary <brian.oleary15@gmail.com>
2018-09-18 13:07:45 -0400

    Initial Commit

---
+++ tutorTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tutorTracking
+//
+//  Created by Brian Thomas O'Leary on 9/18/18.
+//  Copyright © 2018 Brian Thomas O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ tutorTrackingTests/tutorTrackingTests.swift
@@ -0,0 +1,34 @@
+//
+//  tutorTrackingTests.swift
+//  tutorTrackingTests
+//
+//  Created by Brian Thomas O'Leary on 9/18/18.
+//  Copyright © 2018 Brian Thomas O'Leary. All rights reserved.
+//
+
+import XCTest
+@testable import tutorTracking
+
+class tutorTrackingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
