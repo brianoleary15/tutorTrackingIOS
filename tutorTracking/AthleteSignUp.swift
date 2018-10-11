ed9feeb75f8dbd48cd42a440e1f7c84d2e37b8d0
Anthony Kapiti <anthony.kapiti@gmail.com>
2018-10-11 01:43:16 -0400

    Minor update, same as previous commit for the most part

---
+++ tutorTracking/AthleteSignUp.swift
@@ -0,0 +1,30 @@
+//
+//  AthleteSignUp.swift
+//  tutorTracking
+//
+//  Created by Anthony Kapiti on 10/11/18.
+//  Copyright © 2018 Brian Thomas O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class AthleteSignUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
